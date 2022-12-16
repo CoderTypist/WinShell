7fe83a43ee8abc87953b0d87f96f6445b1066bab
CoderTypist <cjbargraser@gmail.com>
2022-12-15 20:20:55 -0600

    Initial implementation (base class needed)

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from tokenize import SimpleTokenizer
+from tokenize import CommandTokenizer
 
 
 def main():
@@ -7,7 +7,15 @@ def main():
 
 if __name__ == '__main__':
 
-    tokenizer = SimpleTokenizer()
+    text = './script.py arg1 "arg with spaces" \'another arg with spaces\' arg4'
+
+    try:
+        tokens = CommandTokenizer.tokenize(text)
+        for t in tokens:
+            print(f'- {t}')
+    except Exception as e:
+        print(e)
+
 
     main()
 
--- tokenize/commandTokenizer.py
+++ tokenize/commandTokenizer.py
@@ -9,7 +9,7 @@ class CommandToken:
         if not match:
             raise Exception('Error: class CommandToken: __init__(): match is None')
 
-        self.text = match.groups()[0]
+        self.text = match.group(0)
         self.length = len(self.text)
 
 
@@ -17,31 +17,43 @@ class CommandTokenizer:
 
     regex_double_quoted_text = re.compile(r'".*?"')
     regex_single_quoted_text = re.compile(r"'.*?'")
-    regex_text = re.compile(r'[a-zA-Z]+[a-zA-Z0-9]*')
-    regex_whitespace = re.compile(r'[\s\t]+')
+    regex_text = re.compile(r'[^\s\'\"]+')
+    regex_whitespace = re.compile(r'[\s\n]+')
 
-    def tokenize(self, text: str) -> List[str]:
+    @classmethod
+    def tokenize(cls, text: str) -> List[str]:
 
-        if not text:
-            raise Exception('Error: class CommandTokenizer: tokenize(): text is None')
+        assert(isinstance(text, str))
+        assert(len(text) > 0)
+        if len(text) == 0:
+            return []
 
-        length = len(str)
+        length = len(text)
         index = 0
         tokens = []
 
         while True:
 
-            match = self.regex_double_quoted_text.match(text[index::])
-            if match:
-                token = CommandTokenizer.extract_token(match)
+            if index == length:
+                break
+
+            remaining_text = text[index::]
+            # tokens to keep
+
+            if (match := CommandTokenizer.regex_text.match(remaining_text)) or \
+               (match := CommandTokenizer.regex_double_quoted_text.match(remaining_text)) or \
+               (match := CommandTokenizer.regex_single_quoted_text.match(remaining_text)):
+
+                token = CommandToken(match)
                 tokens.append(token.text)
                 index += token.length
-                continue
 
-            raise Exception(f'Error: class CommandTokenizer: tokenize(): Unable to tokenize: Invalid character(s): {text[index::]}')
+            # tokens to ignore
+            elif match := CommandTokenizer.regex_whitespace.match(remaining_text):
+                token = CommandToken(match)
+                index += token.length
 
+            else:
+                raise Exception(f'Error: class CommandTokenizer: tokenize(): Unable to tokenize: Invalid character(s): {text[index::]}')
 
-    @classmethod
-    def extract_token(cls, text: str, compiled_regex: re.Pattern) -> Union[None, CommandToken]:
-        pass
-
+        return tokens
